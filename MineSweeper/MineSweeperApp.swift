f008f4e284442b33bcd580a5ea74dd9dba2e41fb
ParadiseDevil <93950967+ParadiseDevil@users.noreply.github.com>
2025-04-28 19:04:58 +0300

    Initial Commit

---
+++ MineSweeper/MineSweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MineSweeperApp.swift
+//  MineSweeper
+//
+//  Created by Дамир Шаймуханбетов on 28.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct MineSweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
